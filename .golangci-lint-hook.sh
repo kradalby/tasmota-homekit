37d86a6128abb2040573e791c5e801161b64a0bd
Kristoffer Dalby <kristoffer@dalby.cc>
2025-11-18 13:38:15 +0100

    ci: update workflow and tooling

--- .golangci-lint-hook.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Hook script for golangci-lint pre-commit
-# Runs golangci-lint on the entire project (not just changed files)
-
-set -e
-
-# Run golangci-lint on all Go files
-golangci-lint run ./...
